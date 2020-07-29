67674d5deb02fe423409ac0c2b7ca05308b990a2
ersaumya <ersaumyarout@gmail.com>
2020-07-29 23:11:01 +0530

    Db context and jwt setting

---
+++ SecureAPI/Settings/Jwt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecureAPI.Settings
+{
+    /// <summary>
+    /// The class is used to read data of JWT from app.setting.json 
+    /// </summary>
+    public class Jwt
+    {
+        public string Key { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public double DurationInMinutes { get; set; }
+    }
+}
