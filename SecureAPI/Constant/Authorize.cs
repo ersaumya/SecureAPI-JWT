932453b3ae491dcf434935a8e5df22f10dcaf0ff
ersaumya <ersaumyarout@gmail.com>
2020-07-30 19:05:36 +0530

    seeding data

---
+++ SecureAPI/Constant/Authorize.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecureAPI.Constant
+{
+    public class Authorize    {
+        public enum Roles
+        {
+            Admin,
+            User,
+            Guest
+        }
+        public const string default_username = "soumya";
+        public const string default_email = "ersaumyarout@gmail.com";
+        public const string default_password = "Rout@123";
+        public const Roles default_role = Roles.Admin;
+    }
+}
---
+++ SecureAPI/Data/ApplicationDbContextSeed.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using SecureAPI.Models;
+using SecureAPI.Constant;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SecureAPI.Data
+{
+    public class ApplicationDbContextSeed
+    {
+        public static async Task SeedEssentialsAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Roles
+            await roleManager.CreateAsync(new IdentityRole(Authorize.Roles.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Authorize.Roles.User.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Authorize.Roles.Guest.ToString()));
+            //Seed Default User
+            var defaultUser = new ApplicationUser { UserName = Authorize.default_username, Email = Authorize.default_email, EmailConfirmed = true, PhoneNumberConfirmed = true };
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                await userManager.CreateAsync(defaultUser, Authorize.default_password);
+                await userManager.AddToRoleAsync(defaultUser, Authorize.default_role.ToString());
+            }
+        }
+    }
+}
