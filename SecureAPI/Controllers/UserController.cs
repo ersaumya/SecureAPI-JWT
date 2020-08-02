078eacfc0f45f3d5c18c5da6ae571f5c46fbd5fd
ersaumya <ersaumyarout@gmail.com>
2020-08-02 18:38:08 +0530

    store refresh token in cookies

--- SecureAPI/Controllers/UserController.cs
+++ SecureAPI/Controllers/UserController.cs
@@ -33,6 +33,20 @@ namespace SecureAPI.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Function take refresh token and store it in browser cookies
+        /// </summary>
+        /// <param name="refreshToken"></param>
+        private void SetRefreshTokenInCookie(string refreshToken)
+        {
+            var cookieOptions = new CookieOptions
+            {
+                HttpOnly = true,
+                Expires = DateTime.UtcNow.AddDays(10)
+            };
+            Response.Cookies.Append("refreshToken", refreshToken, cookieOptions);
+        }
+
         [HttpPost("addrole")]
         public async Task<IActionResult> AddRoleAsync(AddRole model)
         {
